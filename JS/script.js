ea7b8024d201a365396b189c7f87805e07237e22
Pedro <pedrosaraivadev@gmail.com>
2022-01-17 21:43:56 -0300

    Maior parte do JavaScript concluida

--- JS/script.js
+++ JS/script.js
@@ -18,5 +18,28 @@ function del() {
     document.getElementById('total').innerHTML = total.substring(0, total.length - 1);
 }
 
+// Botão para gerar Raiz Quadrada
+
+function sqrt() {
+    document.getElementById('total').innerHTML = eval(Math.sqrt(total.innerHTML));
+}
+
+// Botões para gerar Potências
+
+// Elevado a 2
+function elevated2() {
+    document.getElementById('total').innerHTML = eval(Math.pow(total.innerHTML, 2));
+}
+
+// Elevado a 10
+function elevated10() {
+    document.getElementById('total').innerHTML = eval(Math.pow(total.innerHTML, 10));
+}
+
 // Calcular e gerar resultado
 
+function calcular() {
+    if (total.textContent != 'Error') {
+        document.getElementById('total').innerHTML = eval(total.innerHTML);
+    }
+}
